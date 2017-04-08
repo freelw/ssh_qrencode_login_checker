89b2c2393253b0e6acbc727eb799ac112539470d
wangli(李望) <wangli@tencent.com>
2017-04-08 17:28:30 +0800

    1

--- entry.js
+++ entry.js
@@ -10,11 +10,29 @@ router.get('/register', register);
 router.get('/query', query);
 
 function query(req, res) {
-    console.log('query');
-    res.end('0');
+    let key = req.query.key;
+    console.log('query key =', key);
+    if (!!register_pool[key]) {
+        res.end('1');
+    } else {
+        res.end('0');
+    }
 }
+
 function register(req, res) {
-    console.log('register');
+    console.log('register : ', req.query.key);
+    register_pool[req.query.key] = Date.now();
     res.end('0');
 }
+
+(function loop() {
+    const timeout_interval = 30000;
+    for (let key in register_pool) {
+        let t = register_pool[key];
+        if (t + timeout_interval < Date.now()) {
+            delete register_pool[key];
+        }
+    }
+    setTimeout(loop, 1000);
+})();
 module.exports = router;
